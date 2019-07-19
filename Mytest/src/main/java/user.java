504bfd1cd73fff90be6dfaba8d9ada533f2e8836
dlh <948465697@qq.com>
2019-07-19 16:46:57 +0800

    qqq

---
+++ Mytest/src/main/java/user.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName user
+ * @Description
+ * @Author 不写代码的程序员
+ * @Date 16:42 2019/7/19
+ * @Version 2.1
+ **/
+
+public class user {
+    private String usernmae;
+    private String age;
+}
